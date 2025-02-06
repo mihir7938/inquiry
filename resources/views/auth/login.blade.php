8cc05e1fa647623ac0f2f56769fab78e0e6a1cef
Mihir Patel <mihirpatel7938@gmail.com>
2025-02-06 18:08:17 +0530

    06-02 la

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -6,6 +6,7 @@
     <meta name="csrf-token" content="{{ csrf_token() }}">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<title>Strong Services | Log in</title>
+	<link rel="icon" href="{{asset('img/favicon.ico')}}" />
 	<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
 	<link rel="stylesheet" href="{{asset('adminlte/css/all.min.css')}}">
 	<link rel="stylesheet" href="{{asset('adminlte/css/adminlte.min.css')}}">
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,6 +6,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="icon" href="{{asset('img/favicon.ico')}}" />
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
     <link rel="stylesheet" href="{{asset('adminlte/css/all.min.css')}}">
     <link rel="stylesheet" href="{{asset('adminlte/css/adminlte.min.css')}}">
